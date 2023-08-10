da3011e661c2f4a30725369633f347a89e8988e1
Mofwork <141851558+Mofwork@users.noreply.github.com>
2023-08-10 08:59:53 -0400

    cM

---
+++ THE MIND GAME.py
@@ -0,0 +1,6 @@
+print("Welcome to mind game")
+name = input('What is your name: ')
+object = input('type in a object in the singular form: ')
+action = input(f'type in a action you would do with the {object}: for  ')
+
+print(f"{name} used the {object} for {action}")
